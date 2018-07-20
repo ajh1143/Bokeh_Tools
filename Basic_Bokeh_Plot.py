1a8ac459557d63f2311ede15d0e91ddd3c3596a6
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-07-20 00:30:22 -0700

    Update Basic_Bokeh_Plot.py

--- Basic_Bokeh_Plot.py
+++ Basic_Bokeh_Plot.py
@@ -1,8 +1,12 @@
 from bokeh.plotting import figure
 from bokeh.io import output_file, show
 
+
+#Class to create a basic Bokeh plot, allows user to overlay a second set of data
 class BasicPlot():
 
+    
+    #Creates a basic figure, specify an x-axis label, y-axis label and figure width
     def figure_generator(self, xlab, ylab, width):
         """
         Args: xlab = label for x-axis
@@ -12,7 +16,8 @@ class BasicPlot():
         fig = figure(x_axis_label = xlab, y_axis_label = ylab, plot_width = width)
         return fig
        
-       
+        
+    #Creates a set of circle values to populate the figure, and x/y data sets to be plotted   
     def circle_values(figure, xdata, ydata):
         """
         Args:  figure = basic figure generated with figure_generator
@@ -23,6 +28,7 @@ class BasicPlot():
         return figure.circle(x=xdata, y=ydata)
     
     
+    #Creates a set of x values to be populated as an overlay with the circle points, passed new x and y data sets
     def x_values(figure, xdata, ydata):
         """
         Args:  figure = basic figure generated with figure_generator
@@ -33,6 +39,7 @@ class BasicPlot():
         return figure.circle(x=xdata, y=ydata)
         
         
+    #Generates the visual output, pass the created figure glyph, and the output html location    
     def viz_output(glyph, page):
         """
         Args: glyph = the generated bokeh plot
